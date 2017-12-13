2bea92b762fc18d0b811d3d4bc49f99db7c7682d
SupeDeDupe <sethompson@upei.ca>
2017-12-12 23:14:37 -0400

    Setup the function for creating falling rocks

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -82,9 +82,6 @@ const int col[8] = {
   A5, A4, 25, 7, 27, 5, 4, A2
 };
 
-// 2-dimensional array of pixels:
-int pixels[8][8];
-
 // cursor position:
 int x = 5;
 int y = 5;
@@ -106,21 +103,15 @@ void setup() {
     pinMode(row[thisPin], OUTPUT);
     // take the col pins (i.e. the cathodes) high to ensure that the LEDS are off:
     digitalWrite(col[thisPin], HIGH);
-    Serial.begin(9600);
     // set the switch to have a default 20k pullup 
-    pinMode(SWITCH_PIN, INPUT_PULLUP);
-    delay(300);
   }
-/*
-  // initialize the pixel matrix:
-  for (int x = 0; x < 8; x++) {
-    for (int y = 0; y < 8; y++) {
-      pixels[x][y] = HIGH;
-    }
-  }*/
-
+  
+  Serial.begin(9600);
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  delay(300);
+  
   for (int i = 0; i < 8; i++) {
-    rockCols[i] = -1;
+    rockCols[i] = i;
     rockRows[i] = -1;
   }
 
@@ -204,17 +195,6 @@ void loop() {
   delay(100);
   }*/
 
-void readSensors() {
-  // turn off the last position:
-  //pixels[x][y] = HIGH;
-  // read the sensors for X and Y values:
-  x = 7 - map(analogRead(A0), 0, 1023, 0, 7);
-  y = map(analogRead(A1), 0, 1023, 0, 7);
-  // set the new pixel position low so that the LED will turn on in the next
-  // screen refresh:
-  pixels[x][y] = LOW;
-}
-
 void refreshScreen() {
 
    if(millis() - lastMoveTime > AVATAR_SPEED)
@@ -226,50 +206,40 @@ void refreshScreen() {
 
    if(millis() - lastDrawnTime > REDRAW_SPEED)
    {
+    //Serial.println(String(lastItemDrawn));
     clearMatrix();
     switch(lastItemDrawn)
     {
-      case 0:
+      
+      case 0: //rock
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+       if(rockRows[lastItemDrawn] > -1)
+       {
+       digitalWrite(row[rockRows[lastItemDrawn]], HIGH);
+       digitalWrite(col[rockCols[lastItemDrawn]], LOW);
+       }
+       break;
+      case 8:
        digitalWrite(col[lastAvatarCol], HIGH); // sets the LED the avatar was last at to off
        digitalWrite(row[AVATAR_ROW], HIGH);
        digitalWrite(col[avatarCol], LOW); // sets the LED for the new location of the avatar to on
-       break;
-      case 1: //rock
-       digitalWrite(row[rockRows[0]], HIGH);
-       digitalWrite(col[rockCols[0]], LOW);
        break;
       default:
        break;
     }
 
-    if(lastItemDrawn < maxItems-1)
+    if(lastItemDrawn < 9)
       lastItemDrawn++;
     else
       lastItemDrawn = 0;
     lastDrawnTime = millis();
    }
-  
-  
-  /*
-    // iterate over the rows (anodes):
-    for (int thisRow = 0; thisRow < 8; thisRow++) {
-    // take the row pin (anode) high:
-    digitalWrite(row[thisRow], HIGH);
-    // iterate over the cols (cathodes):
-    for (int thisCol = 0; thisCol < 8; thisCol++) {
-      // get the state of the current pixel;
-      int thisPixel = pixels[thisRow][thisCol];
-      // when the row is HIGH and the col is LOW,
-      // the LED where they meet turns on:
-      digitalWrite(col[thisCol], thisPixel);
-      // turn the pixel off:
-      if (thisPixel == LOW) {
-        digitalWrite(col[thisCol], HIGH);
-      }
-    }
-    // take the row pin low to turn off the whole row:
-    digitalWrite(row[thisRow], LOW);
-    }*/
 }
 
 void clearMatrix()
@@ -286,8 +256,8 @@ void clearMatrix()
 
 void createRock()
 {
-  rockRows[0] = 0;
-  rockCols[0] = 0;
+  int newRockIndex = random(0, 8);
+  rockRows[newRockIndex] = 0;
 }
 
 void updateRocks()
@@ -300,16 +270,14 @@ void updateRocks()
       if(rockRows[i] > -1)
         rockRows[i]++;
       if(rockRows[i] > 7)
-        rockRows[i] = 0;
+      {
+        rockRows[i] = -1;
+        createRock();
+      }
     }
 
     lastRockUpdate = millis();
    }
-
-   
-   //digitalWrite(row[rockRows[0]], LOW); // sets the LED the avatar was last at to off
-   //digitalWrite(row[rockRows[0]], LOW);
-   //digitalWrite(col[rockCols[0]], LOW); // sets the LED for the new location of the avatar to on
 }
 
 void updateAvatarPosition()
@@ -337,6 +305,5 @@ void updateAvatarPosition()
   }
   else
     forward = !forward;
-  //readSensors();
 }
 
